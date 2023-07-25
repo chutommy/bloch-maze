d65aa2c92182abba89fde12ab4aea5a72e4a6244
Tommy Chu <chutommy@fit.cvut.cz>
2023-07-25 19:03:23 +0200

    Encapsulate rendering

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pygame
 
 from game import Game, GameState
 from level import levels
-from render import render_game, render_cell, render_player
+from render import Render
 
 game = Game(levels[0])
 
@@ -17,11 +17,12 @@ DELAY = 0.08
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Bloch's Maze")
+pygame.display.set_caption("Bloch Maze")
 clock = pygame.time.Clock()
 
-oldRect = render_game(screen, game, CELL_SIZE, game.level.start_state, game.level.end_state, game.level.start_state)
-pygame.display.flip()
+render = Render(screen, CELL_SIZE)
+oldRect = render.render_game(game, game.level.start_state, game.level.end_state, game.level.start_state)
+pygame.display.update()
 
 running = True
 while running:
@@ -34,9 +35,8 @@ while running:
         running = False
     elif game.state == GameState.FAIL:
         game.reset()
-        oldRect = render_game(screen, game, CELL_SIZE, game.level.start_state, game.level.end_state,
-                              game.level.start_state)
-        pygame.display.flip()
+        oldRect = render.render_game(game, game.level.start_state, game.level.end_state, game.level.start_state)
+        pygame.display.update()
 
     prev = game.player
     key = pygame.key.get_pressed()
@@ -54,8 +54,8 @@ while running:
     else:
         continue
 
-    render_cell(screen, game.maze, prev.x, prev.y, CELL_SIZE, game.level.start_state, game.level.end_state)
-    newRect = render_player(screen, game.maze, game.player, CELL_SIZE, game.player_state)
+    render.render_cell(game.maze, prev.x, prev.y, game.level.start_state, game.level.end_state)
+    newRect = render.render_player(game.maze, game.player, game.player_state)
     pygame.display.update(oldRect)
     pygame.display.update(newRect)
     oldRect = newRect
