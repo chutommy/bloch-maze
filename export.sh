1a221e4bb09c581f32a3790aa65986b3015a5a14
Tommy Chu <chutommy@fit.cvut.cz>
2023-07-28 16:11:09 +0200

    Update export file

--- export.sh
+++ export.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-cxfreeze src/main.py --compress --target-name "bloch-maze" --target-dir "dist" --silent > /dev/null
-cp -r assets dist
-cp LICENSE dist
+
+TARGET_NAME="bloch-maze"
+TARGET="dist"
+
+declare -a COMPONENTS=("assets" "config.json" "LICENSE" "requirements.txt")
+
+cxfreeze main.py --compress --target-name "${TARGET_NAME}" --target-dir "${TARGET}"
+
+for FILE in "${COMPONENTS[@]}"; do
+  cp -r "${FILE}" "${TARGET}"
+  echo $FILE
+done
